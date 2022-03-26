00bdba5dd93ab7887facc65380b49379ee60de80
Franco Casanova Salirrosas <85380588+CasanovaFranco@users.noreply.github.com>
2022-03-25 20:39:53 -0500

    Creacion de una clase y objeto student

---
+++ Semana01/src/main/java/pe/edu/upc/Program.java
@@ -0,0 +1,17 @@
+package pe.edu.upc;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Student student = new Student();
+		
+		student.setFirstName("Franco Casanova Salirrosas");
+		
+		
+		System.out.println(student.getFirstName());
+
+	}
+
+}
